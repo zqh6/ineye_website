8baf925df274752ef6d35d138265b5e769955b9c
luolin <luolinae86@gmail.com>
2017-05-11 11:01:56 +0800

    将各个模块独立出来

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#index"
+
+  resources :homes,    only: [:index]
+  resources :medicals, only: [:index]
+  resources :centers,  only: [:index]
+  resources :experts,  only: [:index]
+  resources :sciences, only: [:index]
+  resources :news,     only: [:index]
+  resources :kepus,    only: [:index]
 end
