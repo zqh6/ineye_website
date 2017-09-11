9e1dadb4d6aabf3a266e1b598e5948f8481a85f7
yangjinfeng <15884576209@163.com>
2017-09-11 11:17:45 +0800

    test

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 config.assets.precompile += %w( mobile_test.css )
+config.assets.precompile += %w( da.css )
