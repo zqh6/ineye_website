c9e1a5bd5792304f80742bb24dca07a8c95b336f
yangjinfeng <1154813185@qq.com>
2017-11-16 14:39:23 +0800

    add module

---
+++ app/controllers/con_education_controller.rb
@@ -0,0 +1,2 @@
+class ConEducationController < ApplicationController
+end
---
+++ test/controllers/con_education_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ConEducationControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
