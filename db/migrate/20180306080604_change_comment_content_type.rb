62ff38fc9509081fd7bdfd5879421ddf5758be3d
mytxz@foxmail.com <mytxz@foxmail.com>
2018-03-06 17:00:22 +0800

    1, Add.

--- db/migrate/20180306080604_change_comment_content_type.rb
+++ db/migrate/20180306080604_change_comment_content_type.rb
@@ -1,5 +1,5 @@
 class ChangeCommentContentType < ActiveRecord::Migration[5.0]
   def change
-    change_column :comments, :content, :text
+    change_column :comments, :content, :text, limit: 107772150
   end
 end
