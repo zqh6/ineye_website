6457fc35440229c31eaf1e203f25f55c13aa01bd
mytxz@foxmail.com <mytxz@foxmail.com>
2018-03-14 14:27:06 +0800

    1, Seel all user.

--- app/controllers/administration/v1/users_controller.rb
+++ app/controllers/administration/v1/users_controller.rb
@@ -18,9 +18,11 @@ class Administration::V1::UsersController < Administration::V1::PrivilegedContro
   def index
     user = User.included_by(session[:user]['id']).first
     @users = User.alive.not_role_code('1')
+=begin
     if session[:user]['role_code']!='1'
       @users = @users.create_user_is(user)
     end
+=end
     @users = @users.phone_number_like(params[:phone_number]) if params[:phone_number].present?
     @users = @users.name_like(params[:name])                 if params[:name].present?
     @users = @users.reorder('created_at DESC').paginate(page: params[:page], per_page: 20)
