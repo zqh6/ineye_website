d9368c9230ae0e36b133cc6436e053c0249942f0
mytxz@foxmail.com <mytxz@foxmail.com>
2018-03-16 14:22:34 +0800

    1, Fix jquery problem in doctor show.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,4 +1,7 @@
 class DoctorsController < ApplicationController
+
+  layout 'content'
+
   def show
     @user = User.find(params[:id])
     redirect_to '/' and return if @user.role_code!='common_user'
