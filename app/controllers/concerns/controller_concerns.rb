640d7f2e77c934d56fd3d49ccb9234adc7a19e30
mytxz@foxmail.com <mytxz@foxmail.com>
2018-04-16 15:52:01 +0800

    1, Add tag_name like about doctor_flag. 2, Give placeholder to video_url and static_url.

--- app/controllers/concerns/controller_concerns.rb
+++ app/controllers/concerns/controller_concerns.rb
@@ -11,11 +11,19 @@ module ControllerConcerns
         tag_arr.each do |tag_name|
           tag_name = tag_name.to_s.strip
           if tag_name.present?
-            tag_relation = TagRelation.new relation_type: object.class.name.underscore, relation_id: object.id, tag_name: tag_name, tag_flag: (user_names.include?(tag_name) ? 'doctor_name' : '')
+            tag_relation = TagRelation.new relation_type: object.class.name.underscore, relation_id: object.id, tag_name: tag_name, tag_flag: (check_string_has_array_one(tag_name, user_names) ? 'doctor_name' : '')
             tag_relation.save!
           end
         end if tag_arr.present?
       end
+    end
+
+    def check_string_has_array_one(string, array)
+      result = false
+      array.each do |i|
+        result = true if string.include?(i)
+      end
+      result
     end
 
   end
