c81d71d3962ba824f4fafc466ab034feea11262a
mytxz@foxmail.com <mytxz@foxmail.com>
2018-04-16 13:49:21 +0800

    1, Finish notice new.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,8 +5,8 @@ class HomesController < ApplicationController
   end
 
   def index
-    @new = New.alive.classify_is_not('notice').order('occurred_at DESC').limit(7)
-    @new_notice = New.alive.classify_is('notice').order('occurred_at DESC').limit(7)
+    @new = New.alive.where('aim_at_platform in (?)', ['pc', 'pc_and_mobile']).classify_is_not('notice').order('occurred_at DESC').limit(7)
+    @new_notice = New.alive.where('aim_at_platform in (?)', ['pc', 'pc_and_mobile']).classify_is('notice').order('occurred_at DESC').limit(7)
     @doctors = User.alive.role_code_is('common_user').reorder('user_order DESC')
     render layout: "content", action: :index_backup2 and return
   end
