ff64bf92833ff8592b855f6c3221ad017daf6f93
mytxz@foxmail.com <mytxz@foxmail.com>
2018-05-02 12:05:14 +0800

    1, Add.

--- db/migrate/20180502040154_change_url_value_long.rb
+++ db/migrate/20180502040154_change_url_value_long.rb
@@ -1,5 +1,5 @@
 class ChangeUrlValueLong < ActiveRecord::Migration[5.0]
   def change
-    change_column :url_counts, :url, :string, limit: 1000
+    change_column :url_counts, :url, :string, limit: 500
   end
 end
