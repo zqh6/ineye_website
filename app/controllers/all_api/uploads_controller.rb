1df3c6b3da7ca5e9e16dc9a5b899e7b302f60b89
mytxz@foxmail.com <mytxz@foxmail.com>
2018-05-15 14:58:58 +0800

    1, Add.

--- app/controllers/all_api/uploads_controller.rb
+++ app/controllers/all_api/uploads_controller.rb
@@ -15,13 +15,25 @@ class AllApi::UploadsController < AllApi::PresentationController
         oss_path = AliyunOss.instance.put(oss_dir, File.open(file.tempfile), {'content_type': file.content_type})
         path = oss_path
       elsif params[:local]=='true'
-        relative_path = '/uploads/'+ params[:path].to_s+'/'+Time.new.strftime('%Y_%m')+'/'+SecureRandom.uuid
+        time_str = Time.new.strftime('%Y_%m')
+        uuid = SecureRandom.uuid
+
+        relative_path = '/uploads/'+ params[:path].to_s+'/'+time_str+'/'+uuid
         local_path = Rails.root.to_s+'/public'+relative_path
         FileUtils.makedirs local_path
         local_path = local_path+'/'+file.original_filename
         relative_path = relative_path +'/'+file.original_filename
         FileUtils.cp file.tempfile.path, local_path
         FileUtils.chmod 0777, local_path
+
+        relative_path = '/uploads/'+ params[:path].to_s+'/'+time_str+'/'+uuid
+        local_path2 = Yetting.m_ineye_website['rails_root']+'/public'+relative_path
+        FileUtils.makedirs local_path2
+        local_path2 = local_path2+'/'+file.original_filename
+        relative_path = relative_path +'/'+file.original_filename
+        FileUtils.cp file.tempfile.path, local_path2
+        FileUtils.chmod 0777, local_path2
+
         path = relative_path
       end
       collection.push({
