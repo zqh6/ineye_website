9a6eaa9142e5933936af5c94018bcb7fdb84d94e
mytxz@foxmail.com <mytxz@foxmail.com>
2018-09-29 09:59:27 +0800

    1, Add content query about news.

--- app/models/new.rb
+++ app/models/new.rb
@@ -2,9 +2,10 @@ class New < ApplicationRecord
 
   belongs_to :user
 
-  scope :classify_is, ->(classify) { where "#{table_name}.classify = :classify ", classify: classify  }
+  scope :classify_is,        ->(classify) { where "#{table_name}.classify = :classify ", classify: classify  }
   scope :scan_rails_path_is, ->(scan_rails_path) { where "#{table_name}.scan_rails_path = :scan_rails_path ", scan_rails_path: scan_rails_path  }
-  scope :classify_is_not, ->(classify) { where "#{table_name}.classify != :classify ", classify: classify  }
-  scope :title_like,  ->(title)    { where "#{table_name}.title LIKE :title",     title: "%#{title}%" }
+  scope :classify_is_not,    ->(classify) { where "#{table_name}.classify != :classify ", classify: classify  }
+  scope :title_like,         ->(title)    { where "#{table_name}.title LIKE :title", title: "%#{title}%" }
+  scope :content_like,       ->(content)  { where "#{table_name}.content LIKE :content", content: "%#{content}%" }
 
 end
