819c5cfd376c70ea93e42ba6cb40567246e441c5
mytxz@foxmail.com <mytxz@foxmail.com>
2019-01-15 10:34:31 +0800

    1, Add.

--- app/controllers/administration/v1/activity_enters_controller.rb
+++ app/controllers/administration/v1/activity_enters_controller.rb
@@ -8,10 +8,14 @@ class Administration::V1::ActivityEntersController < Administration::V1::Privile
     end
     if params[:export_file]=='true'
       csv = CSV.generate do |csv|
-        csv << [ (ActivityEnter.human_attribute_name :name), (ActivityEnter.human_attribute_name :phone_number), (ActivityEnter.human_attribute_name :created_at), (ActivityEnter.human_attribute_name :activity_type)]
+        csv << [ (ActivityEnter.human_attribute_name :name), (ActivityEnter.human_attribute_name :phone_number), (ActivityEnter.human_attribute_name :created_at), (ActivityEnter.human_attribute_name :activity_type), '预约时间', '年龄']
+        names = []
         @activity_enters.each do |activity_enter|
-          row_data =[activity_enter.name, activity_enter.phone_number, activity_enter.created_at_desc, ShareEnum.activity_types[activity_enter.activity_type.to_sym]]
-          csv << row_data
+          if names.exclude?(activity_enter.name)
+            row_data =[activity_enter.name, activity_enter.phone_number, activity_enter.created_at_desc, ShareEnum.activity_types[activity_enter.activity_type.to_sym], activity_enter.pre_time.try(:strftime, '%Y-%m-%d %H:%M:%S'), activity_enter.age]
+            csv << row_data
+            names.push(activity_enter.name)
+          end
         end
       end.encode('UTF-8')
       send_data csv, filename: '活动报名'+Time.now.strftime('%Y-%m-%d %H-%M-%S')+'.csv' and return
